35ec7aec1a14f624041790a1bb62e6384b1a9caa
Nathan Morley <ndmorley1641@gmail.com>
2016-09-05 22:08:20 -0600

    Experimenting with some spritesheets and grass tiles.

--- Roguelike/Main.js
+++ Roguelike/Main.js
@@ -22,9 +22,14 @@ var stage = null;
 var queue = null;
 var inputManager = new InputManager();
 
+var tileset = null;
 var airhorn = null;
 
 var manifest = [
+	{
+		src: "Images/Tileset.png",
+		id: "tileset"
+	},
 	{
 		src: "Images/airhorn.png",
 		id: "airhorn"
@@ -41,8 +46,8 @@ function initialize(canvasID){
 	
 	stage = new createjs.Stage(canvas);
 	
-	stage.canvas.width = 1910;
-	stage.canvas.height = 1070;
+	stage.canvas.width = 1920;
+	stage.canvas.height = 1080;
 	
 	stage.enableMouseOver();
 	
@@ -69,6 +74,48 @@ function loadAssets(){
 }
 
 function loadComplete(){
+	tileset = new createjs.SpriteSheet(
+		{
+			images: [queue.getResult("tileset")],
+			frames: {
+				width: 32,
+				height: 32
+			},
+			animations: {
+				grass_0: 0,
+				grass_1: 1,
+				grass_2: 2
+			}
+		}
+	);
+	
+	var testTile = new createjs.Sprite(tileset);
+	
+	for(var i = 0; i < 60; i++){
+		for(var j = 0; j < 34; j++){
+			var tile = testTile.clone();
+			tile.x = 32 * i;
+			tile.y = 32 * j;
+			
+			var frame = (Math.random() < 0.33) ? "grass_0" : "grass_1";
+			
+			var randomValue = Math.random();
+			var frame = "grass_0";
+			
+			if(randomValue < 0.33){
+				frame = "grass_0";
+			}else if(randomValue > 0.33 && randomValue < 0.66){
+				frame = "grass_1";
+			}else if(randomValue> 0.66){
+				frame = "grass_2";
+			}
+			
+			tile.gotoAndStop(frame);
+			
+			stage.addChild(tile);
+		}
+	}
+	
 	airhorn = new createjs.Bitmap(queue.getResult("airhorn"));
 	airhorn.x = stage.canvas.width / 2;
 	airhorn.y = stage.canvas.height / 2;
@@ -84,21 +131,20 @@ function loadComplete(){
 
 function update(){
 	if(airhorn){
-		
 		if(inputManager.isKeyDown(KEYCODE.NUMPAD_8) || inputManager.isKeyDown(KEYCODE.NUMPAD_7) || inputManager.isKeyDown(KEYCODE.NUMPAD_9)){
-			airhorn.y -= 5;
+			airhorn.y -= 32;
 		}
 		
 		if(inputManager.isKeyDown(KEYCODE.NUMPAD_2) || inputManager.isKeyDown(KEYCODE.NUMPAD_1) || inputManager.isKeyDown(KEYCODE.NUMPAD_3)){
-			airhorn.y += 5;
+			airhorn.y += 32;
 		}
 		
 		if(inputManager.isKeyDown(KEYCODE.NUMPAD_4) || inputManager.isKeyDown(KEYCODE.NUMPAD_7) || inputManager.isKeyDown(KEYCODE.NUMPAD_1)){
-			airhorn.x -= 5;
+			airhorn.x -= 32;
 		}
 		
 		if(inputManager.isKeyDown(KEYCODE.NUMPAD_6) || inputManager.isKeyDown(KEYCODE.NUMPAD_9) || inputManager.isKeyDown(KEYCODE.NUMPAD_3)){
-			airhorn.x += 5;
+			airhorn.x += 32;
 		}
 	}
 	
